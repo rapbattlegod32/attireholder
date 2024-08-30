804981eb2dbef33b299ff58937311b4296c48563
alex <70163067+rapbattlegod32@users.noreply.github.com>
2024-08-30 10:48:11 +0100

    minor changes

--- commands/user/profile.js
+++ commands/user/profile.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const { getProfile, getGroups, getBadges, playerPicture, seePremium, fetchCollectibles, checkIfPlayerExists, isThereAPrimaryGroup } = require('../../functions/functions.js');
-const Canvas = require('canvas');
 
 module.exports = { 
     name: "profile",
@@ -24,18 +23,6 @@ module.exports = {
         let { groupslength } = await getGroups(args[0]);
         let { sum, amountofcollectibles } = await fetchCollectibles(args[0]);
         let { primaryboolean, role } = await isThereAPrimaryGroup(args[0]);
-
-        const canvas = Canvas.createCanvas(6480, 720);
-        const context = canvas.getContext("2d");
-        // const avatar = await Canvas.loadImage("C:\\Users\\Alexu\\Desktop\\attireholder\\media\\noFilter.png");
-        context.drawImage(avatar, 25, 25 , 720, 720);
-        context.beginPath();
-        context.arc(125, 125, 100, 0, Math.PI * 2, true);
-        context.closePath();
-        context.clip();
-        context.strokeStyle = "#74037b";
-        context.strokeRect(0, 0, canvas.width, canvas.height);
-        const attatchment = new Discord.MessageAttachment(canvas.toBuffer(),"friendimagelist.png");
 
         const embed = new Discord.MessageEmbed()
         .setTitle(`Profile Info(${args[0]})`)
