24ba3ec31eee8d0fcf68c6f9190c95cdcb96e506
alex <70163067+rapbattlegod32@users.noreply.github.com>
2024-08-30 13:01:13 +0100

    Delete commands directory

--- commands/other/ping.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports = {
-    name: "ping",
-    description: "Ping pong!",
-    execute(message){
-        message.channel.send('pong')
-    }
-}
