b69387488c1368f7417b4ba3dfbcd15c975295c8
alex <70163067+rapbattlegod32@users.noreply.github.com>
2024-09-15 15:54:22 +0100

    fixed getid command + getIdFromUsername function finally

--- commands/roblox/getid.js
+++ commands/roblox/getid.js
@@ -10,5 +10,13 @@ module.exports = {
 			.setDescription('username to get ID from')
 			.setRequired(true)),
 	async execute(interaction) {
+		const input = interaction.options.getString("username");
+		const result = await getIdFromUsername(input);
+		if (result) {
+			const { id, hasVerifiedBadge, displayName, name } = result;
+			await interaction.reply(`User ID: **\`${id}\`**\nUsername: **\`${name}\`**\nDisplay Name: **\`${displayName}\`**\nVerified Badge: **\`${hasVerifiedBadge}\`**`);
+		} else {
+			await interaction.reply('Error: getIdFromUsername returned null. (user doesnt exist)');
+		}
 	},
 };
--- functions/userFunctions.js
+++ functions/userFunctions.js
@@ -1,27 +1,34 @@
 const { discordaccount: { token, prefix }, robloxaccount: { robloseccookie, groupid } } = require('../config.json');
-const fetch = require('node-fetch');
+const axios = require('axios');
 
-async function getIdFromUsername(username){
+async function getIdFromUsername(username) {
     try {
-        const body = {
+        const response = await axios.post('https://users.roblox.com/v1/usernames/users', {
             usernames: [
                 username
             ],
             excludeBannedUsers: false
-        };
-        
-        const response = await fetch(`https://users.roblox.com/v1/usernames/users`, {
-            method: 'POST',
-            body: JSON.stringify(body),
-            headers: {'Content-Type': 'application/json'}
         });
-        
+
+        if (response.data && response.data.data && response.data.data[0]) {
+            var id = response.data.data[0].id; var hasVerifiedBadge = response.data.data[0].hasVerifiedBadge; var displayName = response.data.data[0].displayName; var name = response.data.data[0].name;
+            return {
+                id,
+                hasVerifiedBadge,
+                displayName,
+                name
+            };
+        } else {
+            return null;
+        }
     } catch (error) {
-        console.log(error);
+        console.error("Error during API request:", error);
+        return null;
     }
 }
 
+
+
 module.exports = {
-    getIdFromUsername,
-    getUsernameFromID
+    getIdFromUsername
 }
